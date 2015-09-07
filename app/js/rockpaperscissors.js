40801141548fde9c5a43cf9e37ba0631fd46c973
Andrew Mestas <andrew.mestas.dev@gmail.com>
2015-09-06 23:06:42 -0700

    fixed bugs and added quit feature

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -3,8 +3,25 @@
 ////////////////////////////////////////////////
 
 function getInput() {
-    return prompt("Please choose either 'rock', 'paper', or 'scissors'.");
+  var input;
+  do{
+    input = prompt("Please choose either 'rock', 'paper', or 'scissors'.");
+  }while(!check(input));
+  return input;
+  
 }
+function check(input){
+  switch(input)
+    {
+      case "rock" :
+      case "paper" :
+      case "scissors" :
+      case "quit" :
+       return true;
+      default : alert("Please enter rock, paper, or scissors");
+       return false;
+    }
+  }
 function randomPlay() {
     var randomNumber = Math.random();
     if (randomNumber < 0.33) {
@@ -35,35 +52,37 @@ function getComputerMove(move) {
 
 function getWinner(playerMove,computerMove) {
   var winner;
-  winner = "computer";
-  
+  winner = "Computer";
+  if(playerMove == "quit")
+    return playerMove;
   if(playerMove.length == computerMove.length)
      winner = "tie";
   else{
     if(playerMove == "rock")
     {
       if(computerMove == "scissors")
-       winner = "player";
+       winner = "Player";
     }
     else if(playerMove == "paper")
     {
       if(computerMove == "rock")
-       winner = "player";
+       winner = "Player";
     }
     else if(playerMove == "scissors")
     {
       if(computerMove == "paper")
-       winner = "player";
+       winner = "Player";
     }
     else
-      winner = "computer";
+      winner = "Computer";
   }
   
     return winner;
 }
 
 function playTo(playUntil) {
-    alert("Let's play Rock, Paper, Scissors. \nThe person who wins " + playUntil + " rounds will be declared the champion!");
+    alert("Let's play Rock, Paper, Scissors. \nThe person who wins " + playUntil + " rounds will be declared the champion! \nEnter quit to exit at any time.");
+ 
     var playerWins = 0;
     var computerWins = 0;
     var tie = 0;
@@ -74,17 +93,29 @@ function playTo(playUntil) {
   do{
      alert("Player: " + playerWins + " Computer: " + computerWins + " Tie: " + tie);
  result = getWinner(getPlayerMove(getInput()), getComputerMove(randomPlay()));
+   if(result == "quit")
+     alert("Exiting Now!");
+   else{   
      alert(result == "tie" ? "Tie match..." : result + " wins this round!");
- if(result == "player") 
-  playerWins+=1;
- else if (result == "computer")
-  computerWins+=1;
- else 
-  tie+=1;
-    }while(playerWins <= playUntil && computerWins <= playUntil);
-  alert(playerWins == playUntil ? "Player" : "Computer" + " is the champion!" );
+     if(result == "Player") 
+       playerWins+=1;
+     else if (result == "Computer")
+       computerWins+=1;
+     else 
+       tie+=1;
+    } 
+  }while(playerWins != playUntil && computerWins != playUntil && result != "quit");
+  alert((playerWins == playUntil ? "Player" : "Computer") + " is the champion!" );
     return [playerWins, computerWins];
 }
+function testNum(){
+  var choice;
+  
+  do{
+    choice = prompt("How many rounds of rock paper scissors do you want to play?");
+  }while(isNaN(choice));
+  return choice;
+}
 
-var choice = prompt("How many rounds of rock paper scissors do you want to play?");
+var choice = testNum();
 playTo(choice);
--- app/js/rockpaperscissors.js~
+++ app/js/rockpaperscissors.js~
@@ -1,12 +1,9 @@
 ////////////////////////////////////////////////
 /*   Provided Code - Please Don't Edit   */
 ////////////////////////////////////////////////
-'use strict';
 
 function getInput() {
-    console.log("Please choose either 'rock', 'paper', or 'scissors'.")
-   
-    return prompt();
+    return prompt("Please choose either 'rock', 'paper', or 'scissors'.");
 }
 function randomPlay() {
     var randomNumber = Math.random();
@@ -25,23 +22,22 @@ function randomPlay() {
 function getPlayerMove(move) {
   if(move === null)
    move = getInput();
-  console.log("Player " + move);
+  alert("Player chose " + move);
   return move;
 }
 
 function getComputerMove(move) {
     if(move === null)
      move = randomPlay();
-    console.log("Comp " + move);
+    alert("Computer chose " + move);
     return move;
 }
 
 function getWinner(playerMove,computerMove) {
-    var winner;
-    // Write code that will set winner to either 'player', 'computer', or 'tie' based on the values of playerMove and computerMove.
-    // Assume that the only values playerMove and computerMove can have are 'rock', 'paper', and 'scissors'.
-    // The rules of the game are that 'rock' beats 'scissors', 'scissors' beats 'paper', and 'paper' beats 'rock'.
-  if(playerMove == computerMove)
+  var winner;
+  winner = "computer";
+  
+  if(playerMove.length == computerMove.length)
      winner = "tie";
   else{
     if(playerMove == "rock")
@@ -66,25 +62,29 @@ function getWinner(playerMove,computerMove) {
     return winner;
 }
 
-function playToFive() {
-    console.log("Let's play Rock, Paper, Scissors");
+function playTo(playUntil) {
+    alert("Let's play Rock, Paper, Scissors. \nThe person who wins " + playUntil + " rounds will be declared the champion!");
     var playerWins = 0;
     var computerWins = 0;
     var tie = 0;
     var result;
 
     // Write code that plays 'Rock, Paper, Scissors' until either the player or the computer has won five times.
-   do{
-     console.log("Player: " + playerWins + " Computer " + computerWins + " Tie: " + tie);
+//noprotect
+  do{
+     alert("Player: " + playerWins + " Computer: " + computerWins + " Tie: " + tie);
  result = getWinner(getPlayerMove(getInput()), getComputerMove(randomPlay()));
+     alert(result == "tie" ? "Tie match..." : result + " wins this round!");
  if(result == "player") 
-  playerWins+=1
+  playerWins+=1;
  else if (result == "computer")
   computerWins+=1;
  else 
   tie+=1;
-    }while(playerWins <= 5 && computerWins <= 5);
+    }while(playerWins <= playUntil && computerWins <= playUntil);
+  alert(playerWins == playUntil ? "Player" : "Computer" + " is the champion!" );
     return [playerWins, computerWins];
 }
 
-playToFive();
+var choice = prompt("How many rounds of rock paper scissors do you want to play?");
+playTo(choice);
